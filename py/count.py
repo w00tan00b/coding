c013b8e24b5b223eb99b4028bfa1a9a4574dfde4
w00tan00b <41906753+w00tan00b@users.noreply.github.com>
2018-07-30 23:31:47 +0200

    Initial commit

---
+++ py/count.py
@@ -0,0 +1,32 @@
+print("Hello")
+
+count = 1
+while count < 11:
+    print(count)
+    count = count + 1
+if count == 11:
+    print("Counting complete")
+
+import random as random
+
+
+print(random.randint(1, 5))
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(list[2])
+
+print(list[4:6])
+
+
+dict = {
+    "Hai": "Fisch",
+    "Wal": "Säugetier",
+    "Seehund": "Säugetier",
+    "Walhai": "Fisch"
+}
+
+print(dict["Hai"])
+
+
